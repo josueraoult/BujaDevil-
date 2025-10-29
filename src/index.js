89052981acd07750d6c0edc265e7aaba85fb6f0f
josueraoult <103897395+josueraoult@users.noreply.github.com>
2025-10-29 04:03:16 +0200

    Set up React 18 entry point in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
+// Point d'entrée React 18+ (compatible Vercel, Firebase, etc.)
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
